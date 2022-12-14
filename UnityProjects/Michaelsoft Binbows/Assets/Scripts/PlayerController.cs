59974602b422a899fe88e69c8a1007b632c6d44e
JASPER1010101 <lporter.1.2223@tech4maine.org>
2022-12-14 10:29:29 -0500

    PlayerControllerWIP - V0.1

--- UnityProjects/Michaelsoft Binbows/Assets/Scripts/PlayerController.cs
+++ UnityProjects/Michaelsoft Binbows/Assets/Scripts/PlayerController.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private CharacterController controller;
-    private 
+    private RigidBody2D rb2D;
+    private float moveSpeed;
+    private float jumpForce;
+    private bool isJumping;
+    private float moveHorizontal;
+    private float moveVertical;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2D = gameObject.GetComponent<RigidBody2D>();
+
+        moveSpeed = 3f;
+        jumpForce = 60f;
+        isJumping = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveVertical = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        if(moveHorizontal > 0.1f || moveHorizontal < -0.1f)
+        {
+            rb2D.AddForce(new Vector2(moveHorizontal * moveSpeed, 0f), ForceMode2D.Impulse);
+        }
     }
 }
