2ed110d8dea0995ef78a2f19b883e8be7a172be5
JASPER1010101 <lporter.1.2223@tech4maine.org>
2022-12-19 10:09:28 -0500

    Jasper - Player Movement (Jumping) Complete V0.1.2

--- UnityProjects/Michaelsoft Binbows/Assets/Scripts/PlayerController.cs
+++ UnityProjects/Michaelsoft Binbows/Assets/Scripts/PlayerController.cs
@@ -35,5 +35,26 @@ public class PlayerController : MonoBehaviour
         {
             rb2D.AddForce(new Vector2(moveHorizontal * moveSpeed, 0f), ForceMode2D.Impulse);
         }
+
+        if (!isJumping && moveVertical > 0.1f)
+        {
+            rb2D.AddForce(new Vector2(0f, moveVertical * jumpForce), ForceMode2D.Impulse);
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Platform")
+        {
+            isJumping = false;
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Platform")
+        {
+            isJumping = true;
+        }
     }
 }
