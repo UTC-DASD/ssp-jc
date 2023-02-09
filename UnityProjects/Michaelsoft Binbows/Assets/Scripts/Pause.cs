82eb051bd51c854ca9a0aa4d6a299d8663b04161
Cody101010 <cmcgraw.1.2223@tech4maine.org>
2023-02-09 08:15:41 -0500

    :P

--- UnityProjects/Michaelsoft Binbows/Assets/Scripts/Pause.cs
+++ UnityProjects/Michaelsoft Binbows/Assets/Scripts/Pause.cs
@@ -1,20 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Pause : MonoBehaviour{
+public class Pause : MonoBehaviour
+{
+    
+   [SerializeField] private List<GameObject> pauseMenuButtons;
+   bool isPaused = false;
 
-
-    public GameObject pauseButton, pausePanel;
-
-    public void OnPause()
+    // Update is called once per frame
+    void Update()
     {
-        Time.timescale = 0;
-    } 
+        
+        if(Input.GetButtonDown("Cancel"))
+        {
+            PauseUnpause(ref isPaused);
+        }  
 
-    public void OnUnPause()
-    {
-        Time.timescale = 1;
     }
-   
+
+    public void ResumeButton()
+
+    {
+        PauseUnpause(ref isPaused);
+    }
+
+    public void QuitButton()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+    private void PauseUnpause(ref bool pauseState)
+    {
+
+        if(!pauseState)
+        {
+            Time.timeScale = 0.0f;
+            pauseState = true;
+            Cursor.lockState = CursorLockMode.None;
+
+            foreach (GameObject button in pauseMenuButtons)
+            {
+                button.SetActive(true);
+            }
+
+        }
+
+    else
+    {
+        Time.timeScale = 1.0f;
+        pauseState = false;
+
+        foreach (GameObject button in pauseMenuButtons)
+        {
+            button.SetActive(false);
+        }
+    }
+
+    }
 }
